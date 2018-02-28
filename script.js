53556a9ee6584ce45112f044fb86ac98dabc3467
Alexsandrrh <sadovspbsasha@outlook.com>
2018-02-28 20:52:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    setTimeout(function () {
+        $('.logo img').addClass('-animated');
+        $('.line').addClass('-animated');
+    }, 500);
+    setTimeout(function () {
+        $('.second-block h1').addClass('-animated');
+        $('.social').addClass('-animated');
+        $('.title-more').addClass('-visual');
+    }, 7500);
+});
